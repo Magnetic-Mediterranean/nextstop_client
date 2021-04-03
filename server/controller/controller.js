2fd4009937f3c6db86db59f6c57e85e9b18e158d
Dana Walsh <dana.e.walsh@gmail.com>
2021-04-02 17:00:03 -0700

    Implemented server, routes, and controller skeletons

---
+++ server/controller/controller.js
@@ -0,0 +1,14 @@
+module.exports = {
+  flights: (req, res) => {
+
+  },
+  hotels: (req, res) => {
+
+  },
+  experiences:(req, res) => {
+
+  },
+  purchase: (req, res) => {
+
+  }
+}
--- server/server.js
+++ server/server.js
@@ -4,11 +4,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const controller = require('./controller/controller.js');
+
 // Static Middleware
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Parse
 app.use(express.json());
+
+// Routes
+app.get('/flights', controller.flights);
+app.get('/hotels', controller.hotels);
+app.get('/experiences', controller.experiences);
+app.post('/purchase', controller.purchase);
 
 // Listen
 app.listen(port, () => {
