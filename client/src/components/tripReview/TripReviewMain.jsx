03bfc6c9ca76c9f7c86de5346c4de6126287e0ce
Dana Walsh <dana.e.walsh@gmail.com>
2021-04-06 20:00:23 -0700

    Implementing pulling data from localstorage and loading gif

--- client/src/components/tripReview/TripReviewMain.jsx
+++ client/src/components/tripReview/TripReviewMain.jsx
@@ -11,17 +11,70 @@ class TripReviewMain extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      departingFlight: {},
+      returningFlight: {},
+      hotel: {},
+      experiences: {},
+      infoLoaded: false,
     }
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
     // get info from localstorage
+    // const departingFlight = JSON.parse(window.localStorage.getItem('depart'));
+    // console.log('this is departingFlight', departingFlight);
+    // const returningFlight = JSON.parse(window.localStorage.getItem('return'));
+    // console.log('this is returningFlight', returningFlight)
+    // const hotel = JSON.parse(window.localStorage.getItem('hotel'));
+    // const experiences = JSON.parse(window.localStorage.getItem('experiences'));
+
+    // dummy data
+    const departingFlight = {
+      airline: "Delta",
+      departureTime: "2021-08-06T13:05:00",
+      arrivalTime: "2021-08-08T08:30:00",
+      duration: "29H25M",
+      numberOfStops: 1,
+      airports: ["SFO", "LAX", "ERW"],
+      price: "813.53"
+    };
+    const returningFlight = {
+      airline: "Southwest",
+      departureTime: "2021-08-06T18:05:00",
+      arrivalTime: "2021-08-08T05:30:00",
+      duration: "12H25M",
+      numberOfStops: 0,
+      airports: ["SFO", "ERW"],
+      price: "813.53"
+    };
+    this.setState({
+      departingFlight: departingFlight,
+      returningFlight: returningFlight,
+      // hotel: hotel,
+      // experiences: experiences,
+      infoLoaded: true,
+    })
+  }
+
+  handleClick() {
+
+    this.props.next();
   }
 
   render () {
+    const { departingFlight, returningFlight, infoLoaded } = this.state;
     return (
-      <Container style={{overflow: 'auto'}}>
+      <>
+            {!infoLoaded &&
+      (<Container>
+        <div style={{display: "flex", justifyContent:"center"}}>
+          <img src="https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif"/>
+        </div>
+      </Container>
+    )}
+      {infoLoaded &&
+      (<Container style={{overflow: 'auto'}}>
         <Header>
           <BackButton onClick={this.props.back}> Back </BackButton>
           <PageTitle> Trip Confirmation </PageTitle>
@@ -32,7 +85,7 @@ class TripReviewMain extends React.Component {
         <ExperiencesInfo />
         <CostInfo />
         <CheckoutButton
-        onClick={this.props.next}
+        onClick={this.handleClick}
         style={{
           margin: "auto",
           display: "flex",
@@ -41,6 +94,8 @@ class TripReviewMain extends React.Component {
           fontSize: "15px"
           }}> Checkout </CheckoutButton>
       </Container>
+    )}
+    </>
     )
   }
 }
@@ -88,5 +143,7 @@ const CheckoutButton = styled.button`
   border-color: transparent;
   &:hover {
     cursor: pointer;
+    background: #4ECDC4;
+    transition: 0.5s;
   }
 `;
