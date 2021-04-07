48f5653c6ada07ada8dbb3881268b8a7380bf7f0
Shay Rosner <shaymrosner@gmail.com>
2021-04-07 15:13:25 -0700

    Revert "Merge pull request #21 from Magnetic-Mediterranean/tripcheckout"
    This reverts commit 9fb2001dc2c6024db40d9d0b327bb9f5c7899ba8, reversing
    changes made to e8a17c9eae2f555f76979de5e1f8f76015b0b9d9.

--- client/src/components/tripReview/CostInfo.jsx
+++ client/src/components/tripReview/CostInfo.jsx
@@ -1,42 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import SubContainer from '../sharedStyles/subContainer'
 
 const CostInfo = (props) => {
 
   return (
     <CostInfoContainer>
-      <SectionTitle> Cost Breakdown: </SectionTitle>
-      <SubContainer style={{
-        flexDirection: "column",
-        height: "300px",
-      }}>
-        <SubDiv>
-          <NameOfCost>
-            {props.departingFlight.airline}
-          </NameOfCost>
-          <CostAmount>
-            ${props.departingFlight.price}
-          </CostAmount>
-        </SubDiv>
-        <SubDiv>
-          <NameOfCost>
-            {props.returningFlight.airline}
-          </NameOfCost>
-          <CostAmount>
-            ${props.returningFlight.price}
-          </CostAmount>
-        </SubDiv>
-        {/* <SubDiv>
-          <NameOfCost>
-            {props.hotel.name}
-          </NameOfCost>
-          <CostAmount>
-            ${props.hotel.original_price}
-          </CostAmount>
-        </SubDiv> */}
-
-      </SubContainer>
+      <div> Cost Breakdown: </div>
+      <CostDetails>Cost Info</CostDetails>
     </CostInfoContainer>
   )
 }
@@ -47,23 +17,10 @@ const CostInfoContainer = styled.div`
   width: 100%;
 `;
 
-const SectionTitle = styled.div`
-  margin-left: 23.828px;
-`;
-
-const NameOfCost = styled.div`
-  margin-left: 15px;
-  flex-grow: 20;
-`;
-
-const CostAmount = styled.div`
-  margin-left: 75px;
-  flex-grow: 1;
-  margin-right: 20px;
-`;
-
-const SubDiv = styled.div`
-  display: flex;
-  margin: 10px
-
+const CostDetails = styled.div`
+  width: 95%;
+  height: 100px;
+  margin: 10px auto;
+  background: #4ECDC4;
+  border-radius: 8px;
 `;
--- client/src/components/tripReview/FlightInfo.jsx
+++ client/src/components/tripReview/FlightInfo.jsx
@@ -1,117 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import SubContainer from '../sharedStyles/subContainer'
 
-const FlightInfo = ({departingFlight, returningFlight}) => {
-  let stops = 0;
-  const stop = (FligthDetail) => {
-
-    if (FligthDetail.numberOfStops) {
-      let display = FligthDetail.airports.slice(1, FligthDetail.airports.length - 1);
-      stops = display.length;
-      return display;
-    }
-    return "Non-stop";
-  };
-
-  const flightLegs = (array) => {
-    let display = '';
-    for (let i = 0; i < array.length; i++) {
-      display += array[i];
-      if (i !== array.length - 1) {
-        display += '-';
-      }
-    }
-    return display;
-  };
-
-  const airlineIcon = {
-    "UNITED AIRLINES": "icons/UnitedAirline.png",
-    "PHILIPPINE AIRLINES": "icons/PhilippineAirlines.png",
-    "HAWAIIAN AIRLINES": "icons/haiwaiianAirline.png",
-    "Delta": "icons/delta.png",
-    "Southwest": "icons/southwest.png",
-    "American Airline": "icons/AA.png",
-  };
-
-  const convertToTime = (APIdate) => {
-    let date = new Date(APIdate);
-    return date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-  }
+const FlightInfo = (props) => {
 
   return (
     <FlightInfoContainer>
-      <SectionTitle> Flight Details: </SectionTitle>
-      <SubContainer style={{justifyContent: "space-around"}}>
+      <div> Flight Details: </div>
+      <IndividualFlights>
+        <InfoDiv>
 
-      <InnerDiv>
-        {
-          <Icon src={airlineIcon[departingFlight.airline]} />
-        }
-        <p style={{marginTop: "0px"}}>{departingFlight.airline}</p>
-      </InnerDiv>
-
-      <InnerDiv>
-        {stop(departingFlight)}
-        {stops !== 0 && (
-          <InnerP>{stops} stop(s)</InnerP>
-        )}
-      </InnerDiv>
-
-      <InnerDiv style={{
-        marginLeft: "-15px",
-        marginRight: "-15px"
-      }}>
-      <div style={{
-          display:"flex",
-          justifyContent: "center"}}>
-        {flightLegs(departingFlight.airports)}
-        <br />
-        {departingFlight.duration}
-        </div>
-      </InnerDiv>
-
-      <InnerDiv> Depart: {convertToTime(departingFlight.departureTime)} </InnerDiv>
-      <InnerDiv> Arrive: {convertToTime(departingFlight.arrivalTime)} </InnerDiv>
-
-      <InnerP>${departingFlight.price}</InnerP>
-
-      </SubContainer>
-      <SubContainer style={{justifyContent: "space-around"}}>
-
-      <InnerDiv>
-        {
-          <Icon src={airlineIcon[returningFlight.airline]} />
-        }
-        <p style={{marginTop: "0px"}}>{returningFlight.airline}</p>
-      </InnerDiv>
-
-      <InnerDiv>
-        {stop(returningFlight)}
-        {stops !== 0 && (
-          <div>{stops} stop(s)</div>
-        )}
-      </InnerDiv>
-
-      <InnerDiv style={{
-        marginLeft: "-15px",
-        marginRight: "-15px"
-      }}>
-        <div style={{
-          display:"flex",
-          justifyContent: "center"}}>
-        {flightLegs(returningFlight.airports)}
-        <br />
-        {returningFlight.duration}
-        </div>
-      </InnerDiv>
-
-      <InnerDiv> Depart: {convertToTime(returningFlight.departureTime)} </InnerDiv>
-      <InnerDiv> Arrive: {convertToTime(returningFlight.arrivalTime)} </InnerDiv>
-
-      <InnerP>${returningFlight.price}</InnerP>
-
-      </SubContainer>
+        </InfoDiv>
+      </IndividualFlights>
+      <IndividualFlights>Flight back Home</IndividualFlights>
     </FlightInfoContainer>
   )
 }
@@ -122,40 +22,15 @@ const FlightInfoContainer = styled.div`
   width: 100%;
 `;
 
-const SectionTitle = styled.div`
-  margin-left: 23.828px;
-`;
-
-const Icon = styled.img`
-  width: 35px;
-  height: 35px;
-  border-radius: 5px;
-  object-fit: contain;
-`;
-
-const InnerDiv = styled.div`
-  margin: auto;
+const IndividualFlights = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 16.6%;
-  font-family: 'Roboto', sans-serif;
+  width: 95%;
+  height: 100px;
+  margin: 10px auto;
+  background: white;
+  border-radius: 8px;
 `;
 
-const InnerP = styled.p`
-  margin: auto;
-`;
+const InfoDiv = styled.div`
 
-const FlightContainer = styled.div`
-display: flex;
-width: 95%;
-height: 100px;
-margin: 10px auto;
-background: white;
-border-radius: 8px;
-box-shadow: 0 10px 10px -5px #cccc;
-display: flex;
-justify-content: space-around;
-align-items: center;
 `;
-
